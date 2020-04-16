327145c5248f4675181b0b1cbf0a740f48251820
Alexsander <alexsander.psouza@gmail.com>
2020-04-16 11:28:37 -0300

    Adicionando bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <b-alert show>Default Alert</b-alert>
+      <b-alert variant="success" show>Success Alert</b-alert>
     </div>
     <router-view />
   </div>
