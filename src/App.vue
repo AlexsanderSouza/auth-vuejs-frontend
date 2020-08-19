949f132d1873e738b4dee7a7fbdc502e43e53ed3
Alexsander <alexsander.psouza@gmail.com>
2020-08-19 09:47:41 -0300

    layout da página inicial

--- src/App.vue
+++ src/App.vue
@@ -58,16 +58,6 @@ html {
 #app.onmobile {
     padding-left: 50px;
 }
-.sidebar-overlay {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background-color: #000;
-    opacity: 0.5;
-    z-index: 900;
-}
 .app {
     padding: 30px;
 }
--- src/components/menu/sidebar.vue
+++ src/components/menu/sidebar.vue
@@ -5,7 +5,6 @@
         :theme="selectedTheme"
         :show-one-child="true"
         @toggle-collapse="onToggleCollapse"
-        @item-click="onItemClick"
     />
 </template>
 
@@ -152,15 +151,8 @@ export default {
     },
     methods: {
         onToggleCollapse(collapsed) {
-            console.log(collapsed)
             this.$emit('toggleCollapse', collapsed)
             this.collapsed = collapsed
-        },
-        onItemClick(event, item, node) {
-            console.log('onItemClick')
-            console.log(event)
-            console.log(item)
-            console.log(node)
         },
         onResize() {
             if (window.innerWidth <= 767) {
@@ -178,27 +170,6 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600');
-body,
-html {
-    margin: 0;
-    padding: 0;
-}
-body {
-    font-family: 'Source Sans Pro', sans-serif;
-    font-size: 18px;
-    background-color: #f2f4f7;
-    color: #262626;
-}
-#demo {
-    padding-left: 350px;
-    transition: 0.3s ease;
-}
-#demo.collapsed {
-    padding-left: 50px;
-}
-#demo.onmobile {
-    padding-left: 50px;
-}
 .sidebar-overlay {
     position: fixed;
     width: 100%;
@@ -208,20 +179,5 @@ body {
     background-color: #000;
     opacity: 0.5;
     z-index: 900;
-}
-.demo {
-    padding: 50px;
-}
-.container {
-    max-width: 900px;
-}
-pre {
-    font-family: Consolas, monospace;
-    color: #000;
-    background: #fff;
-    border-radius: 2px;
-    padding: 15px;
-    line-height: 1.5;
-    overflow: auto;
 }
 </style>
